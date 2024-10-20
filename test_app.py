1404ab1f1ae6f92f166bec6cd603e5e1a058d034
MaxSch1 <128728934+MaxSch1@users.noreply.github.com>
2024-10-20 16:34:38 -0700

    updated model and fix issues

--- test_app.py
+++ test_app.py
@@ -1,17 +1,17 @@
-import requests
+# import requests
 
-# URL de l'API
-url = "http://localhost:8000/run-agent/"
+# # URL de l'API
+# url = "http://localhost:8000/run-agent/"
 
-# Les données à envoyer dans la requête
-data = {
-    "query": "envoie un email à Corentin dupuis darkk9172@gmail.com pour dire bonjour et ajoute le dans le crm"
-}
+# # Les données à envoyer dans la requête
+# data = {
+#     "query": "envoie un email à Corentin Malmart darkk9172@gmail.com pour dire bonjour et ajoute le dans le crm"
+# }
 
-# Envoi de la requête POST
-response = requests.post(url, json=data, stream=True)
+# # Envoi de la requête POST
+# response = requests.post(url, json=data, stream=True)
 
-# Affichage des étapes au fur et à mesure
-for chunk in response.iter_content(chunk_size=None):
-    if chunk:
-        print(chunk.decode("utf-8"))
+# # Affichage des étapes au fur et à mesure
+# for chunk in response.iter_content(chunk_size=None):
+#     if chunk:
+#         print(chunk.decode("utf-8"))
